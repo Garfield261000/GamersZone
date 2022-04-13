5fdf7f4b0b909b6f37750c40a14aedfdffdc11ff
Garfield261000 <git config --global user.email rishabhpandram26@gmail.com>
2022-04-13 18:05:53 +0530

    ch(filter):added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React,{ useState } from 'react';
 import './App.css';
+import './Components/Card.css'
 import Navbar from './Components/Navbar';
+import Card from './Components/Card';
+import { Games } from './Components/Games';
+
+
 function App() {
+  const [filtereddata,setfiltereddata]=useState(Games)
+
+  const handlefilter = (event) => {
+    const searchtxt=event.target.value
+    const newfilter= Games.filter((value)=> {
+        return value.Gamename.toLowerCase().includes(searchtxt.toLowerCase());
+    });
+    console.log(newfilter)
+    if(searchtxt===""){
+      setfiltereddata(Games)
+    }
+    else{
+      setfiltereddata(newfilter);
+    }
+    console.log(filtereddata)
+    
+  }
   return (
     <>
-      <Navbar/>
-      <div className="backGroundimage"></div>
+      <Navbar />
+      <div className="search">
+          <input className="searchbox" type="text" placeholder=" Search Game..." onChange={handlefilter} />
+      </div>
+      <div className="contain">
+        {filtereddata.map((val, key) => {
+          return (
+            <Card Image={filtereddata[key].Image} Gamename={filtereddata[key].Gamename} Rating={filtereddata[key].Rating} />
+          );
+        })};
+      </div>
+
     </>
   );
 }
