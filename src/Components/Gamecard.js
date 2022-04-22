0741529393b43bf3cd819104d9c36841d29bb749
Garfield261000 <git config --global user.email rishabhpandram26@gmail.com>
2022-04-23 01:10:48 +0530

    change in Gamecard

--- src/Components/Gamecard.js
+++ src/Components/Gamecard.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Gamecard(props) {
     return (
         <div className='my-2'>
-            <div className="card bg-dark bg-opacity-75 border-light" style={{height:"30em"}}>
-                <img src={props.Image} className="card-img-top px-2 py-2 rounded " style={{height:"13em"}} alt="..." />
+            <div className="card bg-dark bg-opacity-75 border-light" >
+                <img src={props.Image} className="card-img-top px-2 py-2 rounded " alt="..." />
                 <div className="card-body">
                     <h5 className="card-title text-center text-light fs-3">{props.Gamename}</h5>
                     <p className="card-text text-center text-light fs-4">Rating: {props.Rating}</p>
